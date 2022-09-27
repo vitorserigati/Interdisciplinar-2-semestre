98309c2eccc9061254713eb9cd3821b57a1a9e14
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2022-09-26 22:42:52 -0300

    Starting project

---
+++ Interdisciplinar/src/main/java/oop/interdisciplinar/Main.java
@@ -0,0 +1,8 @@
+package oop.interdisciplinar;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello World!");
+    }
+}
