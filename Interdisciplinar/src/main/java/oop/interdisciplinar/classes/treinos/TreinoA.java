045618e92fa66a032282678155955d52bb357fcb
Vitor Serigati de Oliveira Basso <79308595+vitorserigati@users.noreply.github.com>
2022-10-09 08:19:50 -0300

    organizando novas classes treino

---
+++ Interdisciplinar/src/main/java/oop/interdisciplinar/classes/treinos/TreinoA.java
@@ -0,0 +1,15 @@
+package oop.interdisciplinar.classes.treinos;
+
+public class TreinoA extends Treino{
+    String[] exercicio;
+    public void removeExercicio(){
+
+    }
+    public String[] showExercicio(){
+        return this.exercicio;
+    }
+    @Override
+    public String info() {
+        return null;
+    }
+}
---
+++ Interdisciplinar/src/main/java/oop/interdisciplinar/classes/treinos/TreinoB.java
@@ -0,0 +1,15 @@
+package oop.interdisciplinar.classes.treinos;
+
+public class TreinoB extends Treino{
+    String[] exercicio;
+    public void removeExercicio(){
+
+    }
+    public String[] showExercicio(){
+        return this.exercicio;
+    }
+    @Override
+    public String info() {
+        return null;
+    }
+}
